6694cb8a3f7b59fd94053e918b02333c14447858
srihaanas <154008151+srihaanas@users.noreply.github.com>
2024-11-23 16:21:41 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import "./index.css"; // Optional CSS file for styling
+// src/index.js
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';  // Make sure to import the CSS
+
+// App logo path
+import logo from '../public/logo.png';  // Make sure the logo file exists in the public folder
 
 ReactDOM.render(
   <React.StrictMode>
+    <div className="logo-container">
+      <img src={logo} alt="App Logo" />
+    </div>
     <App />
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
