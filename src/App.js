f512801480ab030be72a279cfe52b50158de439c
srihaanas <154008151+srihaanas@users.noreply.github.com>
2024-11-23 16:38:06 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,44 @@
 // src/App.js
-import React from 'react';
+// src/App.js
+import React, { useEffect, useState } from 'react';
+import { Auth } from 'aws-amplify';
+import SignUp from './SignUp';
+import SignIn from './SignIn';
+import SignOut from './SignOut';
 
-function App() {
+const App = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  useEffect(() => {
+    const checkUser = async () => {
+      try {
+        await Auth.currentAuthenticatedUser();
+        setIsAuthenticated(true);
+      } catch (error) {
+        setIsAuthenticated(false);
+      }
+    };
+
+    checkUser();
+  }, []);
+
   return (
-    <div className="App">
-      <h1>Welcome to Neighbormate!</h1>
+    <div>
+      <h1>Neighbormate App</h1>
+      {isAuthenticated ? (
+        <div>
+          <h2>Welcome back!</h2>
+          <SignOut />
+        </div>
+      ) : (
+        <div>
+          <SignIn />
+          <SignUp />
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default App;
+
