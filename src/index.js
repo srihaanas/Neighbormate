dee5ba11a1423e84b0a63a2ae5a1774b8543307e
srihaanas <154008151+srihaanas@users.noreply.github.com>
2024-11-24 14:16:50 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
-// src/index.js
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
-import Amplify from 'aws-amplify';
-import awsconfig from './aws-exports';  // Import your AWS configuration
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
-// Configure Amplify with your AWS details
-Amplify.configure(awsconfig);
+// Define the Cognito hosted UI URL
+const hostedUIURL = `https://us-east-1fpnhox09t.auth.us-east-1.amazoncognito.com/login?client_id=d6k56g6h1k5n23k3c7ntpklc&response_type=code&scope=email+openid+phone&redirect_uri=https%3A%2F%2Fmain.dq6ynn49vwg49.amplifyapp.com%2F`;
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// Automatically redirect to the Cognito hosted UI if the user lands on this URL
+if (window.location.href === "https://main.dq6ynn49vwg49.amplifyapp.com/") {
+  window.location.href = hostedUIURL;
+} else {
+  ReactDOM.render(<App />, document.getElementById("root"));
+}
